17278eb0cf7272a0c973b49070710b576bee6aff
tomoya.fushiki <forrussiaworldcup2018@gmail.com>
2019-06-09 13:33:51 +0000

    follow, unfollow

--- resources/views/users/followings.blade.php
+++ resources/views/users/followings.blade.php
@@ -3,8 +3,6 @@
 @section('content')
     <div class="row">
         <aside class="col-sm-4">
-            @include('users.card', ['user' => $user])
-        </aside>            
             <div class="card">
                 <div class="card-header">
                     <h3 class="card-title">{{ $user->name }}</h3>
@@ -16,23 +14,12 @@
             @include('user_follow.follow_button', ['user' => $user])
         </aside>
         <div class="col-sm-8">
-            @include('users.navtabs', ['user' => $user])
             <ul class="nav nav-tabs nav-justified mb-3">
                 <li class="nav-item"><a href="{{ route('users.show', ['id' => $user->id]) }}" class="nav-link {{ Request::is('users/' . $user->id) ? 'active' : '' }}">TimeLine <span class="badge badge-secondary">{{ $count_microposts }}</span></a></li>
-                <li class="nav-item"><a href="#" class="nav-link">Followings</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">Followers</a></li>
+                <li class="nav-item"><a href="{{ route('users.followings', ['id' => $user->id]) }}" class="nav-link {{ Request::is('users/*/followings') ? 'active' : '' }}">Followings <span class="badge badge-secondary">{{ $count_followings }}</span></a></li>
+                <li class="nav-item"><a href="{{ route('users.followers', ['id' => $user->id]) }}" class="nav-link {{ Request::is('users/*/followers') ? 'active' : '' }}">Followers <span class="badge badge-secondary">{{ $count_followers }}</span></a></li>
             </ul>
-            @if (Auth::id() == $user->id)
-                {!! Form::open(['route' => 'microposts.store']) !!}
-                    <div class="form-group">
-                        {!! Form::textarea('content', old('content'), ['class' => 'form-control', 'rows' => '2']) !!}
-                        {!! Form::submit('Post', ['class' => 'btn btn-primary btn-block']) !!}
-                    </div>
-                {!! Form::close() !!}
-            @endif
-            @if (count($microposts) > 0)
-                @include('microposts.microposts', ['microposts' => $microposts])
-            @endif
+            @include('users.users', ['users' => $users])
         </div>
     </div>
 @endsection
